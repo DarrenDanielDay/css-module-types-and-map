6225521a9e9a44b426f4956412bf70a5236731a2
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-12-19 12:49:14 +0800

    feat: Support code generation for a given folder.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as vscode from "vscode";
 import { generateTypesCode } from "./code-generator";
-import { isCSSModule } from "./utils";
+import { askFolder, getCSSModuleFiles, isCSSModule } from "./utils";
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.workspace.onDidSaveTextDocument(async (e) => {
       const { uri } = e;
-      if (isCSSModule(uri)) {
+      if (await isCSSModule(uri)) {
         await generateTypesCode(uri);
       }
     })
@@ -13,10 +13,24 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "css-module-types-and-map.generate",
-      (uri?: vscode.Uri) => {
-        uri = uri ?? vscode.workspace.workspaceFile;
-        if (uri instanceof vscode.Uri && isCSSModule(uri)) {
-          generateTypesCode(uri);
+      async (uri?: vscode.Uri) => {
+        uri = uri ?? vscode.window.activeTextEditor?.document?.uri;
+        if (uri instanceof vscode.Uri && (await isCSSModule(uri))) {
+          await generateTypesCode(uri);
+        } else {
+          vscode.window.showErrorMessage('Given URI is not a css module file (*.module.css).');
+        }
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "css-module-types-and-map.generate-all",
+      async (uri?: vscode.Uri) => {
+        uri = uri instanceof vscode.Uri ? uri : await askFolder(context);
+        if (uri) {
+          const files = await getCSSModuleFiles(uri);
+          await Promise.all(files.map((file) => generateTypesCode(file)));
         }
       }
     )
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as util from "util";
 import * as vscode from "vscode";
+const { fs, getConfiguration } = vscode.workspace;
 export interface Position {
   /**
    * zero-based index
@@ -53,8 +54,60 @@ export async function writeFileText(uri: vscode.Uri, content: string) {
   await vscode.workspace.fs.writeFile(uri, buf);
 }
 
-export function isCSSModule(uri: vscode.Uri) {
-  return path.parse(uri.fsPath).base.endsWith(".module.css");
+export async function isCSSModule(uri: vscode.Uri) {
+  return (
+    path.parse(uri.fsPath).base.endsWith(".module.css") &&
+    (await fs.stat(uri)).type === vscode.FileType.File
+  );
+}
+
+const lastUsedFolder = "css-module-types-and-map.last-used-folder";
+export async function askFolder(context: vscode.ExtensionContext) {
+  const current = vscode.workspace.workspaceFolders?.[0]?.uri;
+  if (!current) {
+    vscode.window.showWarningMessage("Please open a workspace folder.");
+    return undefined;
+  }
+  const input = await vscode.window.showInputBox({
+    value: context.globalState.get(lastUsedFolder) ?? "",
+    prompt: "provide a folder",
+  });
+  if (input) {
+    try {
+      const result = vscode.Uri.joinPath(current, input);
+      const { type } = await fs.stat(result);
+      if (type === vscode.FileType.Directory) {
+        context.globalState.update(lastUsedFolder, input);
+        return result;
+      }
+    } catch (error) {
+      vscode.window.showErrorMessage(`'${input}' is not a valid folder.`);
+      return undefined;
+    }
+  }
+}
+
+export async function getCSSModuleFiles(
+  uri: vscode.Uri
+): Promise<vscode.Uri[]> {
+  const result: vscode.Uri[] = [];
+  async function dfs(uri: vscode.Uri) {
+    const sub = await fs.readDirectory(uri);
+    for (const [child, childType] of sub) {
+      const childUri = vscode.Uri.joinPath(uri, child);
+      if (childType === vscode.FileType.Directory) {
+        await dfs(childUri);
+      }
+      if (childType === vscode.FileType.File && await isCSSModule(childUri)) {
+        result.push(childUri);
+      }
+    }
+  }
+  const { type } = await fs.stat(uri);
+  if (type === vscode.FileType.Directory) {
+    await dfs(uri);
+  }
+  return result;
 }
 
 const output = vscode.window.createOutputChannel("CSS Modules Types & Map");
